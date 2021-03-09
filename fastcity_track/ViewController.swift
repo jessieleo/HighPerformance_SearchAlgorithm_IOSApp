00d0b5e5082070b26ad6bba1000e59d08e5428ed
Seyit Kaya <seyitkaya1@gmail.com>
2021-03-09 08:00:10 +0300

    Initial Commit

---
+++ fastcity_track/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fastcity_track
+//
+//  Created by Seyit Kaya on 9.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
